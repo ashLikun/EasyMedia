3aa14cf8e7bccae74caeba7a3f1964415f7dd6d6
爱生活、李坤 <496546144@qq.com>
2020-07-10 23:22:38 +0800

    优化

--- library/src/main/java/com/ashlikun/media/video/VideoData.java
+++ library/src/main/java/com/ashlikun/media/video/VideoData.java
@@ -2,6 +2,8 @@ package com.ashlikun.media.video;
 
 import android.content.res.AssetFileDescriptor;
 import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.text.TextUtils;
 
 import java.util.HashMap;
@@ -15,7 +17,7 @@ import java.util.Map;
  * 功能介绍：视频播放数据源
  */
 
-public class VideoData {
+public class VideoData implements Parcelable {
     Builder builder;
 
     protected VideoData(Builder builder) {
@@ -94,7 +96,8 @@ public class VideoData {
         return TextUtils.equals(getUri().toString(), obj.getUri().toString()) || TextUtils.equals(getUrl(), obj.getUrl()) || TextUtils.equals(getFileDescriptor().toString(), obj.getFileDescriptor().toString());
     }
 
-    public static class Builder {
+
+    public static class Builder implements Parcelable {
         protected String url;
         protected String title;
         protected Uri uri;
@@ -141,5 +144,79 @@ public class VideoData {
         public VideoData builder() {
             return new VideoData(this);
         }
+
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeString(this.url);
+            dest.writeString(this.title);
+            dest.writeParcelable(this.uri, flags);
+            dest.writeParcelable(this.fileDescriptor, flags);
+            dest.writeInt(this.headers.size());
+            for (Map.Entry<String, String> entry : this.headers.entrySet()) {
+                dest.writeString(entry.getKey());
+                dest.writeString(entry.getValue());
+            }
+        }
+
+        public Builder() {
+        }
+
+        protected Builder(Parcel in) {
+            this.url = in.readString();
+            this.title = in.readString();
+            this.uri = in.readParcelable(Uri.class.getClassLoader());
+            this.fileDescriptor = in.readParcelable(AssetFileDescriptor.class.getClassLoader());
+            int headersSize = in.readInt();
+            this.headers = new HashMap<String, String>(headersSize);
+            for (int i = 0; i < headersSize; i++) {
+                String key = in.readString();
+                String value = in.readString();
+                this.headers.put(key, value);
+            }
+        }
+
+        public static final Creator<Builder> CREATOR = new Creator<Builder>() {
+            @Override
+            public Builder createFromParcel(Parcel source) {
+                return new Builder(source);
+            }
+
+            @Override
+            public Builder[] newArray(int size) {
+                return new Builder[size];
+            }
+        };
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(this.builder, flags);
+    }
+
+    protected VideoData(Parcel in) {
+        this.builder = in.readParcelable(Builder.class.getClassLoader());
+    }
+
+    public static final Creator<VideoData> CREATOR = new Creator<VideoData>() {
+        @Override
+        public VideoData createFromParcel(Parcel source) {
+            return new VideoData(source);
+        }
+
+        @Override
+        public VideoData[] newArray(int size) {
+            return new VideoData[size];
+        }
+    };
 }
